62bf158707a89c1dd32e313c1fd2a53d1f53af1a
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2025-01-13 22:40:31 -0500

    created Book constructor and addbooks func that makes Book objects and adds them to library

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-let test = document.querySelector('body');
+const library = [];
 
-test.style.backgroundColor = '#F00';
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBooks(...args) {
+    while (args.length > 0) {
+        library.push(new Book(...args));
+        for (i = 0; i < 4; i++) {
+            args.shift();
+        }
+    }
+}
+
+addBooks('Book1', 'J. Doe', 200, true, 'Book2', 'W. Knows', 244, false, 'Book3', 'Unkown', 42, true);
+
+// let mainEl = document.querySelector('main');
+// mainEl.textContent = library[0].title;
+console.log(library[2])
